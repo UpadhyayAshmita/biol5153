55291ebb579b52ca9f463b762ee992cb5e4376f8
Ashmita Upadhyay <ashmitau@uark.edu>
2024-03-24 21:55:01 -0500

    added pinnacle slurm script

--- write_pinnacle_slurm.py
+++ write_pinnacle_slurm.py
@@ -1,19 +1,25 @@
-#!/bin/bash
+# Variables
+job_name = "write_pinnacle_slurm"
+partition = "comp72"
+nodes = 1
+time = "72:00:00"
 
-print("#SBATCH --job-name=wirte_pinnacle_slurm")
-print("#SBATCH --partition comp72")
-print("#SBATCH --nodes=1")
-print("#SBATCH --qos comp")
+# Printing the SLURM script with variables
+print("#!/bin/bash")
+print()
+print(f"#SBATCH --job-name={job_name}")
+print(f"#SBATCH --partition={partition}")
+print(f"#SBATCH --nodes={nodes}")
+print("#SBATCH --qos=comp")
 print("#SBATCH --tasks-per-node=32")
-print("#SBATCH --time=72:00:00")
+print(f"#SBATCH --time={time}")
 print("#SBATCH -o example_%j.out")
 print("#SBATCH -e example_%j.err")
 print("#SBATCH --mail-type=ALL")
 print("#SBATCH --mail-user=email@uark.edu")
-
+print()
 print("module purge")
 print("module load intel/18.0.1 impi/18.0.1 mkl/18.0.1")
-
+print()
 print("cd $SLURM_SUBMIT_DIR")
-print("job command here")
-print('beyonce')
+print("# job command here")
